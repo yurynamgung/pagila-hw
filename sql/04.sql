e984c5840046a4dd2d9b388bffc2227e963cba46
Mike Izbicki <mike@izbicki.me>
2021-02-19 22:32:18 -0800

    update hw04

--- sql/04.sql
+++ sql/04.sql
@@ -1,5 +1,5 @@
 /*
  * Count the total number of films whose rental price is less than $1.
  * HINT:
- * Use the count() aggregation function and the rental_price column of the film table.
+ * Use the count() aggregation function and the rental_rate column of the film table.
  */
